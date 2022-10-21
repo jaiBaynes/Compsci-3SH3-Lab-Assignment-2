07f7d67369a93238ea3864f017a56797fe2c5569
jaiBaynes <82232773+jaiBaynes@users.noreply.github.com>
2022-10-21 13:03:46 -0400

    Update time-shm.c
    doesn't work but best I had before CPU died

--- time-shm.c
+++ time-shm.c
@@ -16,27 +16,10 @@
 
 int main (int argc, char *argv[]) {
 
-    struct timeval current_time;
-//long time = gettimeofday(&current_time, NULL); //initializes the time of day as the current time
-
-//establish region of shared memory before child process is forked
-/* the size (in bytes) of shared memory object */
-const int SIZE = 4096;
-/* name of the shared memory object */
-const char *name = "OS";
-/* strings written to shared memory */
-const long *message_0 = gettimeofday(&current_time, NULL);
-/* shared memory file descriptor */
-int fd;
-/* pointer to shared memory obect */
-long *ptr;
-    /* open the shared memory object */
-    fd = shm_open(name, O_RDONLY, 0666);
-    /* memory map the shared memory object */
-    ptr = (long *) mmap(0, SIZE, PROT_READ, MAP_SHARED, fd, 0); //what happens if star is removed?
-
     pid_t pid;
     pid = fork(); //fork child process
+
+    struct timeval current;
 
     if (pid < 0) { /* error occured */
         fprintf(stderr, "Fork Failed");
@@ -53,11 +36,11 @@ long *ptr;
             /* strings written to shared memory */
             //const char *message 0 = "Hello";
             //const char *message 1 = "World!";
-            const char *message_3 = "Lab 2 Practice!"; //possible answer. Read rest for more info
+            const timeval *message_3 = gettimeofday(&current, NULL); //possible answer. Read rest for more info
             /* shared memory file descriptor */
             int fd;
             /* pointer to shared memory obect */
-            char *ptr;
+            timeval *ptr;
                 /* create the shared memory object */
                 fd = shm_open(name,O_CREAT | O_RDWR,0666);
                 /* configure the size of the shared memory object */
@@ -66,31 +49,38 @@ long *ptr;
                 ptr = (char *)
                     mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
                 /* write to the shared memory object */
-                
-                /*
-                sprintf(ptr,"%s",message 0);
-                ptr += strlen(message 0);
-                sprintf(ptr,"%s",message 1);
-                ptr += strlen(message 1);
-                */
 
                 sprintf(ptr,"%s",message_3); //answer
                 ptr += strlen(message_3);
                 
 
-            execlp("/bin/ls","ls",NULL); ///find a way with variables   
+            execlp("/bin/ls",argv,NULL); ///find a way with variables   
             return 0;
         }
         else { /* parent will wait for the child to compete */
             wait(NULL);
-            /* open the shared memory object */
-            fd = shm_open(name, O_RDONLY, 0666);
-            /* read from the shared memory object */
+            
+            /* the size (in bytes) of shared memory object */
+            const int SIZE = 4096;
+            /* name of the shared memory object */
+            const char *name = "OS";
+            /* shared memory file descriptor */
+            int fd;
+            /* pointer to shared memory obect */
+            timeval *ptr;
+                /* open the shared memory object */
+                fd = shm_open(name, O_RDONLY, 0666);
+                /* memory map the shared memory object */
+                ptr = (timeval *) mmap(0, SIZE, PROT_READ, MAP_SHARED, fd, 0);
+                /* read from the shared memory object */
+                //printf("%s",(long *)ptr);
 
-            printf("%ld",(long *)ptr); //what happens if star is removed?
-            /* remove the shared memory object */
-            shm_unlink(name);
-            return 0;
+                printf ("Seconds: %ld ", ptr.tv_sec - gettimeofday(&current, NULL).tv_sec, "Miliseconds: %ld \n", ptr.tv_usec - gettimeofday(&current, NULL).tv_usec);
+                /* remove the shared memory object */
+                shm_unlink(name);
+                return 0;
+
+            //printf("%ld",(long *)ptr); //what happens if star is removed?
 
             //sprintf(ptr, "Elapsed time: %ld", (*message_0 - gettimeofday(&current_time, NULL)));
             // print current_time - end_time
@@ -98,3 +88,6 @@ long *ptr;
     }
     return 0;
 }
+
+//compile with a -lrt at the end
+//page 140 lab report
